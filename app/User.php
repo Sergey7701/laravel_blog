7a35e2df1750dae0d61647834871a51fa20d2351
imper <imper@windows10.mshome.net>
2020-04-21 12:24:40 +0300

    trait HasRoleAndPermission hasn't hardcode tables

--- app/User.php
+++ app/User.php
@@ -5,6 +5,8 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Traits\HasRolesAndPermissions;
+use App\Role;
+use App\Permission;
 
 class User extends Authenticatable
 {
@@ -38,7 +40,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
+    
+    protected $rolesTable       = 'users_roles';
+    protected $permissionsTable = 'users_permissions';
+    
     public function articles()
     {
         return $this->hasMany(Models\Article::class, 'author_id');
