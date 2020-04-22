3da08dfe7433eb2a667249d072160330f02e208c
imper <imper@windows10.mshome.net>
2020-04-22 14:54:03 +0300

    patch Blade custom directive 'role'

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,11 +1,12 @@
 <?php
-
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+
     /**
      * Register any application services.
      *
@@ -23,8 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        view()->composer('layouts.sidebar', function(\Illuminate\View\View $view){      
+        view()->composer('layouts.sidebar', function(\Illuminate\View\View $view) {
             $view->with('tagsCloud', \App\Tag::tagsCloud());
+        });
+        Blade::if('role', function ($role) {
+            return \auth()->check() && !\auth()->user()->hasRole('$role');
         });
     }
 }
