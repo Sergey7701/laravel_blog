bdb9aa702e3fb2a2d5f77bf0e705da552c40ccac
imper <imper@windows10.mshome.net>
2020-06-26 15:42:34 +0300

    xhprof

--- app/Http/Middleware/XHProfMiddleware.php
+++ app/Http/Middleware/XHProfMiddleware.php
@@ -2,7 +2,6 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use XHProfRuns_Default;
 
 class XHProfMiddleware
 {
@@ -14,7 +13,6 @@ class XHProfMiddleware
      * @param  \Closure  $next
      * @return mixed
      */
-
     public function handle($request, Closure $next)
     {
         xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
@@ -23,9 +21,9 @@ class XHProfMiddleware
 
     public function terminate($request, $response)
     {
-        $xhprofData = xhprof_disable();    
+        $xhprofData = xhprof_disable();
         include_once '/var/www/xhprof/xhprof_lib/utils/xhprof_lib.php';
         include_once '/var/www/xhprof/xhprof_lib/utils/xhprof_runs.php';
-        $a = (new XHProfRuns_Default())->save_run($xhprofData, time());
+        $a          = (new \XHProfRuns_Default())->save_run($xhprofData, time());
     }
 }
